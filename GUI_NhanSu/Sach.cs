c6877d6c8888f63b1760514ce6b5ed1276aa4f7b
trang <thuylinhnd97@gmail.com>
2018-06-10 17:46:13 +0700

    dau sach

--- GUI_NhanSu/Sach.cs
+++ GUI_NhanSu/Sach.cs
@@ -30,7 +30,7 @@ namespace GUI_NhanSu
             txtsoluong.Text = "";
             txttentacgia.Text = "";
             txtnamxb.Text = "";
-          
+
         }
         void ShowData()
         {
@@ -48,83 +48,83 @@ namespace GUI_NhanSu
 
         private void btXoa_Click(object sender, EventArgs e)
         {
-            //if (txtMasach.Text.Length != 0)
-            //{
-            //    string query = @"DELETE FROM dbo.NhanVien Where manv='" + txtMasach.Text + "'";
-            //    if (MessageBox.Show("Phòng ban liên quan đến các bảng khác, bạn có chắc muốn xóa dữ liệu liên quan đến phòng ban ??", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
-            //    {
-            //        if (bus_nv.Xoa(txtMasach.Text.Trim()))
-            //        {
-            //            FormatData();
-            //            ShowData();
-            //            MessageBox.Show("Xóa thành công");
-            //        }
-            //        else
-            //        {
-            //            Exception ex = bus_nv.GetEx();
-            //            MessageBox.Show(ex.Message);
-            //            MessageBox.Show("Lỗi");
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Vui lòng chọn");
-            //}
+            if (txtMasach.Text.Length != 0)
+            {
+                string query = @"DELETE FROM dbo.NhanVien Where manv='" + txtMasach.Text + "'";
+                if (MessageBox.Show("Phòng ban liên quan đến các bảng khác, bạn có chắc muốn xóa dữ liệu liên quan đến phòng ban ??", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    if (bus_nv.Xoa(txtMasach.Text.Trim()))
+                    {
+                        FormatData();
+                        ShowData();
+                        MessageBox.Show("Xóa thành công");
+                    }
+                    else
+                    {
+                        Exception ex = bus_nv.GetEx();
+                        MessageBox.Show(ex.Message);
+                        MessageBox.Show("Lỗi");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn");
+            }
         }
 
         private void btSua_Click(object sender, EventArgs e)
         {
-            //DialogResult dr = MessageBox.Show("Bạn có muốn sửa không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-            //if (dr == DialogResult.OK)
-            //{
-            //    if (txtMasach.Text != "" && txtnamxb.Text != "" && txttentacgia.Text != "" && txtTensach.Text != "")
-            //    {
-                    
-            //        SachDao nv = new SachDao(txtMasach.Text.ToString().Trim(), txtTensach.Text.ToString().Trim(), txttentacgia.Text.Trim(),int.Parse(txtsoluong.Text.Trim()), txtnamxb.Text.ToString().Trim());
-            //        if (bus_nv.Sua(nv) == true)
-            //        {
-            //            FormatData();
-            //            ShowData();
-            //            MessageBox.Show("Sửa thành công");
-            //        }
-            //        else
-            //        {
-            //            Exception ex = bus_nv.GetEx();
-            //            MessageBox.Show(ex.Message);
-            //            MessageBox.Show("Có lỗi xảy ra");
-            //        }
-            //    }
-            //    else
-            //        MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
-            //}
+            DialogResult dr = MessageBox.Show("Bạn có muốn sửa không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dr == DialogResult.OK)
+            {
+                if (txtMasach.Text != "" && txtnamxb.Text != "" && txttentacgia.Text != "" && txtTensach.Text != "")
+                {
+
+                    SachDao nv = new SachDao(txtMasach.Text.ToString().Trim(), txtTensach.Text.ToString().Trim(), txttentacgia.Text.Trim(), int.Parse(txtsoluong.Text.Trim()), txtnamxb.Text.ToString().Trim());
+                    if (bus_nv.Sua(nv) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        MessageBox.Show("Sửa thành công");
+                    }
+                    else
+                    {
+                        Exception ex = bus_nv.GetEx();
+                        MessageBox.Show(ex.Message);
+                        MessageBox.Show("Có lỗi xảy ra");
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
+            }
         }
 
         private void btThem_Click(object sender, EventArgs e)
         {
-            //DialogResult dr = MessageBox.Show("Bạn có muốn thêm không ?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-            //if (dr == DialogResult.OK)
-            //{
-            //    if (txtMasach.Text != "" && txtnamxb.Text != "" && txttentacgia.Text != "" && txtTensach.Text != "")
-            //    {
-            //        SachDao sach = new SachDao(txtMasach.Text.ToString().Trim(), txtTensach.Text.ToString().Trim(), txtTensach.Text.Trim(), int.Parse(txtsoluong.Text.Trim()), txtnamxb.Text.ToString().Trim());
-            //        //(txtMaNV.Text.ToString().Trim(), txtTen.Text.ToString().Trim(), txtMaPhong.Text.ToString().Trim(),DateTime.Parse(dtNgaySinh.Text.ToString()),gioitinh,int.Parse(txtLuong.Text.ToString()), txtDiaChi.Text.ToString().Trim());
-            //        if (bus_nv.Them(sach) == true)
-            //        {
-            //            FormatData();
-            //            ShowData();
-            //            MessageBox.Show("Thêm thành công");
-            //        }
-            //        else
-            //        {
-            //            Exception ex = bus_nv.GetEx();
-            //            MessageBox.Show(ex.Message);
-            //            MessageBox.Show("Có lỗi xảy ra");
-            //        }
-            //    }
-            //    else
-            //        MessageBox.Show("Bạn cần nhập đủ thông tin ");
-            //}
+            DialogResult dr = MessageBox.Show("Bạn có muốn thêm không ?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dr == DialogResult.OK)
+            {
+                if (txtMasach.Text != "" && txtnamxb.Text != "" && txttentacgia.Text != "" && txtTensach.Text != "")
+                {
+                    SachDao sach = new SachDao(txtMasach.Text.ToString().Trim(), txtTensach.Text.ToString().Trim(), txtTensach.Text.Trim(), int.Parse(txtsoluong.Text.Trim()), txtnamxb.Text.ToString().Trim());
+                    //(txtMaNV.Text.ToString().Trim(), txtTen.Text.ToString().Trim(), txtMaPhong.Text.ToString().Trim(),DateTime.Parse(dtNgaySinh.Text.ToString()),gioitinh,int.Parse(txtLuong.Text.ToString()), txtDiaChi.Text.ToString().Trim());
+                    if (bus_nv.Them(sach) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        MessageBox.Show("Thêm thành công");
+                    }
+                    else
+                    {
+                        Exception ex = bus_nv.GetEx();
+                        MessageBox.Show(ex.Message);
+                        MessageBox.Show("Có lỗi xảy ra");
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin ");
+            }
         }
 
         private void btThoat_Click(object sender, EventArgs e)
@@ -143,24 +143,24 @@ namespace GUI_NhanSu
 
         private void btTimKiem_Click(object sender, EventArgs e)
         {
-            //if (txtTimKiem.Text != "")
-            //{
-            //    btHienThi.Enabled = true;
-            //    if (bus_nv.GetDataTimKiem(txtTimKiem.Text.Trim()) != null)
-            //    {
-            //        FormatData();
-            //        dgvSach.DataSource = bus_nv.GetDataTimKiem(txtTimKiem.Text.Trim());
-            //        MessageBox.Show("Tìm thành công");
-            //    }
-            //    else
-            //    {
-            //        Exception ex = bus_nv.GetEx();
-            //        MessageBox.Show(ex.Message);
-            //        MessageBox.Show("Lỗi rồi bạn ơi !");
-            //    }
-            //}
-            //else
-            //    MessageBox.Show("Bạn cần nhập thông tin để tìm kiếm !");
+            if (txtTimKiem.Text != "")
+            {
+                btHienThi.Enabled = true;
+                if (bus_nv.GetDataTimKiem(txtTimKiem.Text.Trim()) != null)
+                {
+                    FormatData();
+                    dgvSach.DataSource = bus_nv.GetDataTimKiem(txtTimKiem.Text.Trim());
+                    MessageBox.Show("Tìm thành công");
+                }
+                else
+                {
+                    Exception ex = bus_nv.GetEx();
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show("Lỗi rồi bạn ơi !");
+                }
+            }
+            else
+                MessageBox.Show("Bạn cần nhập thông tin để tìm kiếm !");
         }
 
         private void dgvSach_CellClick(object sender, DataGridViewCellEventArgs e)
